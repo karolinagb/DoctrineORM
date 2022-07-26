999bbf7f920aa82e7839137dfa0c823d4770db28
x <karolina_bp@hotmail.com>
2022-07-26 11:32:56 -0300

    Buscando alunos com findAll, find, findBy e findOneBy

--- commands/criar-aluno.php
+++ commands/criar-aluno.php
@@ -15,7 +15,9 @@ $entityManager = $entityManagerFactory->getEntityManager();
 //persist = coloca a entidade como observada, se em algum momento nós formos no banco, ela será adicionada
 $entityManager->persist($aluno);
 //Tudo que estiver de modificação depois ele vai continuar observando e vai salvar no banco quando formos efetivamente lá
-$aluno->setNome('Karolina Gomes Bento');
+//$argv[1] = valores dos argumentos, recebe os valores, a partir do índice 1, de tudo que você passar na linha de comando
+//Para inserir alunos dessa forma digitamos no terminal de comando php commands\criar-aluno.php "Nome"
+$aluno->setNome($argv[1]);
 
 //flush = quando terminamos nossas alterações e queremos salvar no banco
 $entityManager->flush();
