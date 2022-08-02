62ae2faebd6cea28ff8de4aa0150f5119051108a
x <karolina_bp@hotmail.com>
2022-08-02 11:14:23 -0300

    Inserindo um aluno

--- commands/criar-aluno.php
+++ commands/criar-aluno.php
@@ -3,21 +3,20 @@
 use Alura\Doctrine\Entity\Aluno;
 use Alura\Doctrine\Helper\EntityManagerFactory;
 
-// require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// $aluno = new Aluno();
-// $aluno->setNome('Karolina Bento');
+// gerenciar a entidade para que ela seja persistida no banco
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
 
-//gerenciar a entidade para que ela seja persistida no banco
-// $entityManagerFactory = new EntityManagerFactory();
-// $entityManager = $entityManagerFactory->getEntityManager();
+$aluno = new Aluno('Karolina Bento');
 
-//persist = coloca a entidade como observada, se em algum momento nós formos no banco, ela será adicionada
-// $entityManager->persist($aluno);
-//Tudo que estiver de modificação depois ele vai continuar observando e vai salvar no banco quando formos efetivamente lá
-//$argv[1] = valores dos argumentos, recebe os valores, a partir do índice 1, de tudo que você passar na linha de comando
-//Para inserir alunos dessa forma digitamos no terminal de comando php commands\criar-aluno.php "Nome"
+// persist = coloca a entidade como observada, se em algum momento nós formos no banco, ela será adicionada
+$entityManager->persist($aluno);
+// Tudo que estiver de modificação depois ele vai continuar observando e vai salvar no banco quando formos efetivamente lá
+// $argv[1] = valores dos argumentos, recebe os valores, a partir do índice 1, de tudo que você passar na linha de comando
+// Para inserir alunos dessa forma digitamos no terminal de comando php commands\criar-aluno.php "Nome"
 // $aluno->setNome($argv[1]);
 
-//flush = quando terminamos nossas alterações e queremos salvar no banco
-// $entityManager->flush();
+// flush = quando terminamos nossas alterações e queremos salvar no banco
+$entityManager->flush();
