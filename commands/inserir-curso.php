f8a9606007cc5a704a764119ff4f6675b70fa580
x <karolina_bp@hotmail.com>
2022-08-04 14:45:35 -0300

    Cadastrando cursos

---
+++ commands/inserir-curso.php
@@ -0,0 +1,14 @@
+<?php
+
+use Alura\Doctrine\Entity\Curso;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+
+$curso = new Curso($argv[1]);
+
+$entityManager->persist($curso);
+$entityManager->flush();
